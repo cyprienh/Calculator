4814c91fec1e4b0bcdaaa04237bf9e057ab32ec7
Cyprien Heusse <c.heusse@gmail.com>
2021-09-09 19:36:47 -0400

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Cyprien Heusse on 09/09/2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
